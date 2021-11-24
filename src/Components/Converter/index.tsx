d654742809901a16b12527e9d6690b824190ba7e
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-25 06:03:41 +0900

    feat: clean up code bases

--- src/Components/Converter/index.tsx
+++ src/Components/Converter/index.tsx
@@ -5,17 +5,20 @@ import './styles.scss'
 const Converter: FC = () => {
   const [nep, setNep] = useState<number>()
   const [busd, setBusd] = useState<number>()
+  const [controller, setController] = useState<string>()
 
   const handleNEP = (e: React.ChangeEvent<HTMLInputElement>): void => {
     let temp: number = parseFloat(e.target.value)
-    setNep(temp)
-    setBusd(temp * 3)
+      setNep(temp)
+      setBusd(temp * 3)
+      setController('nep')
   }
 
   const handleBUSD = (e: React.ChangeEvent<HTMLInputElement>): void => {
     let temp: number = parseFloat(e.target.value)
     setBusd(temp)
-    setNep(temp / 3.00)
+    setNep(temp / 3)
+    setController('busd')
   }
 
   return (
@@ -24,11 +27,11 @@ const Converter: FC = () => {
       <Form className="d-flex flex-column">
         <Form.Group className="mb-1">
           <Form.Label>NEP</Form.Label>
-          <Form.Control value={nep} type="number" onChange={handleNEP} />
+          <Form.Control value={controller == 'nep' ? nep : nep?.toFixed(2)} type="number" onChange={handleNEP} />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>BUSD</Form.Label>
-          <Form.Control value={busd} type="number" onChange={handleBUSD} />
+          <Form.Control value={controller == 'busd' ? busd : busd?.toFixed(2)} type="number" onChange={handleBUSD} />
         </Form.Group>
         <Button variant="outline-primary" className="ms-auto me-auto">Check Wallet Details</Button>
       </Form>
