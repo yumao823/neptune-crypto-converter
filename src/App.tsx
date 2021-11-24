a163d4463248386eaa2ccdc691ccaab34bbb7a8f
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-24 21:06:54 +0900

    feat: add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
